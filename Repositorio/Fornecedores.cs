115be2ee4780b451a26264011c8eedf42c364aa9
Raul <raullalvarenga@hotmail.com>
2017-11-16 21:53:57 -0200

    Criacao da Classe Fornecedores sem os métodos

--- Repositorio/Fornecedores.cs
+++ Repositorio/Fornecedores.cs
@@ -1,7 +1,33 @@
+using Dominio.interfaces;
+using Dominio.classes;
+using System;
+
 namespace Repositorio
 {
-    public class Fornecedores
+    public class Fornecedores : IDominio
     {
-        
+
+        public PJ Empresa { get; set; }
+        public string Categoria { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+
+        public Fornecedores(PJ empresa, string categoria, DateTime dataCadastro)
+        {
+            this.Empresa = empresa;
+            this.Categoria = categoria;
+            this.DataCadastro = dataCadastro;
+
+        }
+
+        public string Cadastrar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Consultar()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
