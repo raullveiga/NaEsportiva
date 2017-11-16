41f2406266213e91157f1533b1ec97edac1e0be2
keikekogake <keike.kogake@gmail.com>
2017-11-16 21:32:31 -0200

    Criação da classe PF.cs

--- Dominio/classes/PF.cs
+++ Dominio/classes/PF.cs
@@ -1,7 +1,29 @@
-namespace Dominio.classes
-{
-    public class PF
-    {
-        
+using System;
+
+namespace Dominio.classes {
+    public class PF {
+        public int Id { get; set; }
+        public string Cpf { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public Endereco Endereco { get; set; }
+
+        public PF () { }
+
+        public PF (int id, string cpf, string nome, string email, string telefone, DateTime dataCadastro, DateTime dataNascimento, Endereco endereco) {
+            this.Id = id;
+            this.Cpf = cpf;
+            this.Nome = nome;
+            this.Email = email;
+            this.Telefone = telefone;
+            this.DataCadastro = dataCadastro;
+            this.DataNascimento = dataNascimento;
+            this.Endereco = endereco;
+
+        }
+
     }
 }
